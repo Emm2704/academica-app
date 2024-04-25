abe6a07ba61213915d2ca40f798dfa3705464ec0
Daniel Diaz <daaniieeliitoodiiaaz@gmail.com>
2024-04-25 00:07:43 -0500

    formularios

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('Cursos/edit');
+});
+
+Route::get('/', function () {
+    return view('Estudiantes/edit');
+});
+
+Route::get('/', function () {
+    return view('Inscripciones/edit');
+});
